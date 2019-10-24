8a72162a8918f5595f920ef4a717e42741417c13
Ardashes <ardoo93@hotmail.com>
2019-10-24 18:29:58 +0400

    practical1

---
+++ homework 1/problem2-Copy1.py
@@ -0,0 +1,10 @@
+course = ('intro to python')
+student = ('hatty potter')
+nickname = ('chocolate expert')
+grade = 100
+python_expert=True
+print ('course:' + course)
+print ('student:'+ student)
+print  ('nickname:' + nickname)
+print ( 'grade:' , grade)
+print ('python_expert:' , python_expert)
